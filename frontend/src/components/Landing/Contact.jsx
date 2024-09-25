bac93065b7af87e2b29e7c297a47562e3bffbc34
gautam-4 <gautamalt468@gmail.com>
2024-09-25 18:25:16 +0530

    update landing page

--- frontend/src/components/Landing/Contact.jsx
+++ frontend/src/components/Landing/Contact.jsx
@@ -16,33 +16,33 @@ function Contact() {
                 <div className="contact_heading" data-aos="flip-up">Our Team</div>
                 <div className="contact_container" data-aos="fade-up">
                     <div className="contact_container-names">
-                        <div className="team-name">Error Overflow</div>
+                        <div className="team-name text-4xl">Error Overflow</div>
                     </div>
                     <div id="carousel" data-aos="fade-up">
                         <figure id="spinner">
-                            <div>Gautam Arora</div>
-                            <div><img src={pfp} alt="" /></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
+                            <div className='backdrop-blur-md bg-opacity-10 bg-white'>Gautam Arora</div>
+                            <div className='backdrop-blur-md bg-opacity-10 bg-white'>Ansh Singhal</div>
+                            <div className='backdrop-blur-md bg-opacity-10 bg-white'>Nikhil Mehlan</div>
+                            <div className='backdrop-blur-md bg-opacity-10 bg-white'>Siddharth M</div>
+                            <div className='backdrop-blur-md bg-opacity-10 bg-white'>Nipun S Nair</div>
                         </figure>
                     </div>
                 </div>
-                <div className='link-icons'>
-                <svg fill="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="35px">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-        <svg fill="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"  height="35px">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-        <svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="35px">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-        <svg fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" viewBox="0 0 24 24" height="35px">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
+                <div className='link-icons flex'>
+                    <svg fill="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="35px">
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                    <svg fill="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="35px">
+                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                    <svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" height="35px">
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                    <svg fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" viewBox="0 0 24 24" height="35px">
+                        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                        <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                    </svg>
                 </div>
                 <div className='mail'>agrilink@gmail.com</div>
             </section>
--- frontend/src/components/Landing/Hero.jsx
+++ frontend/src/components/Landing/Hero.jsx
@@ -4,7 +4,7 @@ import overlayImg from '@/assets/overlay.png';
 function Hero() {
   return (
     <section className="hero flex flex-col lg:flex-row h-[90vh] relative">
-      <div className="hero_overlay bg-secondary opacity-80 flex-1 lg:max-w-[450px] relative overflow-hidden">
+      <div className="hero_overlay bg-secondary opacity-80 flex-1 lg:max-w-[450px] relative hidden sm:block">
         <img 
           src={overlayImg} 
           alt="" 
@@ -12,15 +12,15 @@ function Hero() {
         />
       </div>
       <div className="hero_main flex-2 relative">
-        <div className="hero_main-title title pt-[75px] pl-[80px]">
+        <div className="hero_main-title title pt-[75px] pl-0 sm:pl-[80px] text-">
           Empowering Farmers Through Decentralization
         </div>
-        <div className="hero_main-description mx-[90px] mt-[30px] text-[1.25em] font-[var(--font-three)] text-secondary">
+        <div className="hero_main-description mx-[90px] mt-[30px] text-[1.25em] font-[var(--font-three)] text-secondary hidden sm:block">
           Empowering farmers through decentralized solutions and sustainable practices. 
           Join us in revolutionizing agriculture for a brighter, more resilient future. 
           Together, let's cultivate abundance while nurturing our planet.
         </div>
-        <div className="hero_main-caption absolute bottom-0 bg-[var(--accent-light)] w-full h-[120px] rounded-tr-[10px] p-[10px] ml-[5px] text-[1.08em] opacity-90">
+        <div className="hero_main-caption absolute bottom-0 bg-[var(--accent-light)] w-full h-[120px] rounded-tr-[10px] p-[10px] ml-[5px] text-[1.08em] opacity-90  hidden sm:block">
           <p className="first-quote absolute left-[20px]">
             "Where Tradition Meets Innovation in Agriculture's Next Chapter."
           </p>
